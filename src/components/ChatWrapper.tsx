654067ec448f6a194cc8c88f42d9cc07803fbf76
Milkiyas <milkiyashailub@gmail.com>
2024-08-20 22:21:27 +0300

    ui fix

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -11,7 +11,7 @@ const ChatWrapper = ({ sessionId, initialMessages }: { sessionId: string, initia
         initialMessages
     })
     return (
-        <div className='relative min-h bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2'>
+        <div className='relative min-h bg-zinc-800 flex divide-y divide-zinc-700 flex-col justify-between gap-2'>
             <div className='flex-1 text-black bg-zinc-800 justify-between flex flex-col'>
                 <Messages messages={messages} />
             </div>
