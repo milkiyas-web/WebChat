496824032052fb2e94ed4a6fbdd77dae5af0bb40
Milkiyas <milkiyashailub@gmail.com>
2024-08-20 22:19:03 +0300

    session fix

---
+++ src/app/api/chat-stream/route.ts
@@ -0,0 +1,13 @@
+import { ragChat } from "@/lib/rag-chat";
+import { NextRequest } from "next/server";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
+export const POST = async (req: NextRequest) => {
+  const { messages, sessionId } = await req.json();
+  const lastMessage = messages[messages.length - 1].content;
+  const response = await ragChat.chat(lastMessage, {
+    streaming: true,
+    sessionId,
+  });
+  return aiUseChatAdapter(response);
+  console.log("response", response);
+};
